d0ccc9019e1525ff74e7971f60fd0a0d6b3f0986
Lisa Trevis <l_trevis@hotmail.com>
2018-05-07 07:54:01 -0700

    additional info

--- js/main.js
+++ js/main.js
@@ -65,6 +65,60 @@
 // fileSummary(savedFile, { color: 'red' }) // returns "The red file repost.jpg is of size 14404."
 
 
+// Destructuring arrays
+// const companies = [
+// 	'Google',
+// 	'Facebook',
+// 	'Uber'
+// ];
+
+// To destructure elements, we use square braces:
+// const [ name, name2, name3 ] = companies
+
+// To destructure properties, we use curly braces:
+// const { length } = companies // length = 3
+
+
+// Can also use the rest operator:
+// const [ ...rest ] = companies
+// rest = [ 'Google', Facebook', 'Uber' ]
+
+
+// Destructuring arrays & objects at the same time (not commonly used)
+// const companies = [
+// 	{ name: 'Google', location: 'Mountain View' },
+// 	{ name: 'Facebook', location: 'Menlo Park' },
+// 	{ name: 'Uber', location: 'San Francisco' },
+// ];
+
+// ES5: var location = companies[0].location
+// Calling location returns 'Mountain View'
+
+// ES6:
+// const [{ location }] = companies;
+// Calling location returns 'Mountain View'
+// Goes from outside in, so [] pulls the first item (in this case an object) in the array, and then the word location grabs the value of the location property.
+
+
+// const Google = {
+// 	locations: [ 'Mountain View', 'London', 'New York' ]
+// };
+
+// const { locations } = Google;
+// Calling locations returns the array
+
+// const { locations: [ locations ] } = Google;
+// Calling locations returns 'Mountain View'
+
+
+// Let's say we want to have a function that saves a username and password to a database:
+
+function signup(username, password, email, dateOfBirth, city) {
+	// create new user
+};
+
+signup('myname', 'mypassword', 'me@gmail.com', '6/13/1977', 'Alameda');
+
 
 
 
